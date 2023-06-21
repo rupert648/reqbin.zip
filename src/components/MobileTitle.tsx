fcb3addbd1b83e9cf1b7a5a117728040daf2183a
rupert648 <rupertcarr@hotmail.co.uk>
2023-06-21 21:36:19 +0100

    Created 404 page

--- src/components/MobileTitle.tsx
+++ src/components/MobileTitle.tsx
@@ -4,7 +4,7 @@ export const MobileTitle = () => {
   const { push } = useRouter();
   return (
     <h1
-      className="mx-5 mb-5 cursor-pointer text-6xl font-extrabold lg:hidden"
+      className="mx-5 mb-5 cursor-pointer text-4xl font-extrabold lg:hidden"
       onClick={() => {
         void push("/");
       }}
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,16 @@
+import { Title } from "~/components/Title";
+import { TopIcons } from "~/components/TopIcons";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <main>
+      <div className="flex min-h-screen items-center justify-center bg-gradient-to-b ">
+        <Title />
+        <h3>Paste Bin Not found 🧐</h3>
+      </div>
+      <div className="mx-5 -mt-10">
+        <TopIcons />
+      </div>
+    </main>
+  );
 }
