4bf6e0bd6061e51090ebc918a202c66e377551ec
rupert648 <rupertcarr@hotmail.co.uk>
2024-03-18 19:20:01 +0000

    chore: re-add 404

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import { Title } from "~/components/Title";
+import { TopIcons } from "~/components/TopIcons";
+
+export default function Custom404() {
+  return (
+    <main>
+      <div className="flex min-h-screen items-center justify-center bg-gradient-to-b ">
+        <Title />
+        <h3>Paste Bin Not found 🧐</h3>
+      </div>
+      <div className="mx-5 -mt-10">
+        <TopIcons />
+      </div>
+    </main>
+  );
+}
